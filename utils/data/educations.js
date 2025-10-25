37edd5b1cdec8e1b6101495ac2831996add1b060
MK Al-Amin <77953447+mkalaminit@users.noreply.github.com>
2025-10-25 20:46:50 +0600

    first commit

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,5 +1,5 @@
 export const educations = [
-  {
+  /* {
     id: 1,
     title: "Bachelor Degree",
     duration: "2020 - Present",
@@ -16,5 +16,5 @@ export const educations = [
     title: "Secondary School Certificate",
     duration: "2008 - 2018",
     institution: "Baitus Saif Islamia Madrasah",
-  }
+  }*/
 ]
